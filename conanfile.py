9954267567af1f61c4f0a6e33bf43c6fed3fe590
Dean Moldovan <dean0x7d@gmail.com>
2021-01-15 15:46:08 +0100

    Update conanfile.py to expose individual CMake targets

--- conanfile.py
+++ conanfile.py
@@ -6,12 +6,13 @@
 # members to help us debug any problems that arise.
 
 from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conans.errors import ConanInvalidConfiguration, ConanException
 from conans.model.version import Version
 
 
 class AbseilConan(ConanFile):
     name = "abseil"
+    version = "20200923"
     url = "https://github.com/abseil/abseil-cpp"
     homepage = url
     author = "Abseil <abseil-io@googlegroups.com>"
@@ -24,28 +25,47 @@ class AbseilConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     def configure(self):
-        if self.settings.os == "Windows" and \
-           self.settings.compiler == "Visual Studio" and \
-           Version(self.settings.compiler.version.value) < "14":
-            raise ConanInvalidConfiguration("Abseil does not support MSVC < 14")
+        minimal_cpp_standard = "11"
+        try:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        except ConanInvalidConfiguration:
+            raise
+        except ConanException:
+            self.output.warn("Unnable to determine the default standard version of the compiler")
 
-    def build(self):
-        tools.replace_in_file("CMakeLists.txt", "project(absl CXX)", "project(absl CXX)\ninclude(conanbuildinfo.cmake)\nconan_basic_setup()")
+        minimal_version = {
+            "Visual Studio": "14",
+        }
+
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration(
+                "%s requires at least %s %s" % (self.name, compiler, minimal_version[compiler]))
+
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTING"] = False
         cmake.configure()
+        return cmake
+
+    def build(self):
+        tools.replace_in_file("CMakeLists.txt", "project(absl CXX)", "project(absl CXX)\ninclude(conanbuildinfo.cmake)\nconan_basic_setup()")
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses")
-        self.copy("*.h", dst="include", src=".")
-        self.copy("*.inc", dst="include", src=".")
-        self.copy("*.a", dst="lib", src=".", keep_path=False)
-        self.copy("*.lib", dst="lib", src=".", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        if self.settings.os == "Linux":
-            self.cpp_info.libs = ["-Wl,--start-group"]
-        self.cpp_info.libs.extend(tools.collect_libs(self))
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.extend(["-Wl,--end-group", "pthread"])
+        self.cpp_info.includedirs = []
+        self.cpp_info.libs = []
+        self.user_info.version = self.version
